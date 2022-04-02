449d31b7557a3ee82c7dcff796b0167491c36371
deshayk <deshaykidd@gmail.com>
2022-04-02 16:44:43 -0500

    import morse

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Learn By Doing - Morse Code Translator by DeShay K.
+Github: https://github.com/deshayk
+Medium: https://www.medium.com/@deshayk
+LinkedIn: https://www.linkedin.com/in/deshayk/
+"""
+
+from morse import morse #import the morse code dictionary
+
